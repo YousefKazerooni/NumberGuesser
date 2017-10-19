6736f8c3356b0569b425ea5edd3b265240db0b4f
YousefKazerooni <gityousefkazerooni@gmail.com>
2017-10-19 18:34:51 -0400

    Initial Commit

---
+++ NumberGuesser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumberGuesser
+//
+//  Created by Yousef Kazerooni on 10/19/17.
+//  Copyright © 2017 YousefKazerooni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
